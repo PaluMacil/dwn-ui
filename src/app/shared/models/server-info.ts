7797501197c72bd67e5b533a7930702dd081dbb5
Dan Wolf <dcwolf@gmail.com>
2018-08-14 22:11:17 -0400

    update site info view for config data reconfiguration

--- src/app/shared/models/server-info.ts
+++ src/app/shared/models/server-info.ts
@@ -1,11 +1,27 @@
 export interface ServerInfo {
+    config: Configuration
+    initializedDate: Date
+    goVersion: string
+}
+
+export interface SetupConfiguration {
+	initialAdmin: string
+}
+
+export interface Configuration {
+	webServer: WebServerConfiguration
+	setup: SetupConfiguration
+	database: DatabaseConfiguration
+}
+
+export interface WebServerConfiguration {
     protocol: string
     host: string
     port: string
     baseURL: string
     uiProxyPort: string
-    valueLogUseMMAP: boolean
-    setup: { initialAdmin: string }
-    initializedDate: Date
-    goVersion: string
+}
+
+export interface DatabaseConfiguration {
+	dataDir: string
 }
