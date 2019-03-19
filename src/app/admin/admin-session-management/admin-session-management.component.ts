d0286d0d489fafe0abe8e6c726d045731f1ee11e
Dan Wolf <dcwolf@gmail.com>
2019-03-18 23:15:27 -0400

    format ipv6 correctly

--- src/app/admin/admin-session-management/admin-session-management.component.ts
+++ src/app/admin/admin-session-management/admin-session-management.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../user/user.service';
 import { SessionDetails } from '../../shared/models/session-details';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-session-management',
@@ -12,15 +13,29 @@ export class AdminSessionManagementComponent implements OnInit {
   sessions = new Array<SessionDetails>();
 
   constructor(
-    private us: UserService
+    private userService: UserService
   ) { }
 
   ngOnInit() {
-    this.us.sessions().subscribe(
-      s => {
-        this.sessions.push(...s);
-      }
-    );
+    this.userService.sessions()
+      .pipe(
+        map(
+          sessionDetails => {
+            return sessionDetails.map(
+              detail => {
+                const ipParts = detail.session.ip.split(':');
+                const ipString = ipParts.length <= 2 ? ipParts[0] : ipParts.slice(0, 3).join(':');
+                detail.session.ip = ipString;
+                return detail;
+              }
+            );
+          }
+        )
+      )
+      .subscribe(
+        s => {
+          this.sessions.push(...s);
+        }
+      );
   }
-
 }
