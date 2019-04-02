d960bddf3a69ca8b99fff2991668c11c6c551fa9
Dan Wolf <dcwolf@gmail.com>
2019-04-01 23:56:22 -0400

    add basic group creation form

--- src/app/admin/admin-group-management/admin-group-management.component.ts
+++ src/app/admin/admin-group-management/admin-group-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupService } from '../group.service';
-import { Group } from '../../shared/models';
+import { Group, GroupCreationRequest, AlertMessage } from '../../shared/models';
+import { faSyncAlt, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, Validators, AbstractControl, ValidatorFn, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-group-management',
@@ -8,12 +10,18 @@ import { Group } from '../../shared/models';
   styleUrls: ['./admin-group-management.component.scss']
 })
 export class AdminGroupManagementComponent implements OnInit {
-
   groups = new Array<Group>();
   selectedGroupName: string;
+  alertMessage: AlertMessage;
+  iconRefresh = faSyncAlt;
+  iconAddGroup = faPlusSquare;
+  loading = false;
+  createGroupForm: FormGroup;
+  showCreateGroupForm = false;
 
   constructor(
-    private gs: GroupService
+    private gs: GroupService,
+    private fb: FormBuilder
   ) { }
 
   selectGroup(group: Group) {
@@ -25,10 +33,46 @@ export class AdminGroupManagementComponent implements OnInit {
     this.groups[idx] = group;
   }
 
-  ngOnInit() {
-    this.gs.groups().subscribe(
-      g => this.groups = g
+  createGroup() {
+    const groupRequest = this.createGroupForm.value as GroupCreationRequest;
+    this.gs.create(groupRequest).subscribe(
+      group => {
+        this.alertMessage = undefined;
+        this.groups.push(group);
+      },
+      err => {
+        this.alertMessage = AlertMessage.fromHttpErrorResponse(err);
+      }
     );
   }
 
+  fetchInfo() {
+    this.loading = true;
+    this.gs.groups().subscribe(
+      g => {
+        this.alertMessage = undefined;
+        this.groups = g;
+      },
+      err => {
+        this.alertMessage = AlertMessage.fromHttpErrorResponse(err);
+      },
+      () => {
+        this.loading = false;
+      }
+    );
+  }
+
+  toggleCreateGroupForm() {
+    this.createGroupForm.reset();
+    this.showCreateGroupForm = !this.showCreateGroupForm;
+  }
+
+  ngOnInit() {
+    this.fetchInfo();
+    this.createGroupForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern(/[^_][A-Z_]+/)]],
+      requires2FA: [false],
+      requiresVaultPIN: [false]
+    });
+  }
 }
