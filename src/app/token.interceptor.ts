390d64091f2b0f1ecddaa10d8e011e76dd6aff68
Dan Wolf <dcwolf@gmail.com>
2019-08-04 22:21:06 -0400

    don't attach a null auth token

--- src/app/token.interceptor.ts
+++ src/app/token.interceptor.ts
@@ -6,19 +6,23 @@ import { environment } from '../environments/environment';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    constructor(public userService: UserService) {}
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      const attachToken = ['danwolf.net', 'visit.danwolf.net', 'staging.danwolf.net']
-        .some(url => url === location.host) || location.hostname === 'localhost';
-      if (!attachToken) {
-        return next.handle(request);
-      }
-      const tokenName = environment.tokenName;
-      request = request.clone({
-        setHeaders: {
-          [tokenName]: `${this.userService.getToken()}`
-        }
-      });
+  constructor(public userService: UserService) { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const attachToken = ['danwolf.net', 'visit.danwolf.net', 'staging.danwolf.net']
+      .some(url => url === location.host) || location.hostname === 'localhost';
+    if (!attachToken) {
       return next.handle(request);
     }
+    const tokenName = environment.tokenName;
+    // Don't attach a null token
+    const token = this.userService.getToken()
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          [tokenName]: `${token}`
+        }
+      });
+    }
+    return next.handle(request);
   }
+}
