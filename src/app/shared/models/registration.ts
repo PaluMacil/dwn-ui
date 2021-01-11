443a1dfe0aae412a194965b5709873e6b731f610
Dan Wolf <dcwolf@gmail.com>
2021-01-10 22:58:45 -0500

    update Angular

--- src/app/shared/models/registration.ts
+++ src/app/shared/models/registration.ts
@@ -13,7 +13,7 @@ export interface UserCreationRequest {
 
 export const validatePasswordsMatch = (control: AbstractControl): { [key: string]: boolean } | null => {
   if (!control.pristine) {
-    const password = control.parent.get('password')?.value; // to get value in input tag
+    const password = control.parent?.get('password')?.value; // to get value in input tag
     const passwordAgain = control.value;
     // the password has been entered and they match
     if (password.length > 0 && password !== passwordAgain) {
